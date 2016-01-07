a90d3e8a9ce74955b373387228600b072106c04c
Seth Silesky <ssilesky@Seths-Tulane-Macbook.local>
2016-01-07 16:51:57 -0600

    changes page background to red

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,9 +1,10 @@
 'use strict';
 
-chrome.runtime.onInstalled.addListener(details => {
-  console.log('previousVersion', details.previousVersion);
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+  // No tabs or host permissions needed!
+  console.log('Turning ' + tab.url + ' red!');
+  chrome.tabs.executeScript({
+    code: 'document.body.style.backgroundColor="red"'
+  });
 });
-
-chrome.browserAction.setBadgeText({text: '\'Allo'});
-
-console.log('\'Allo \'Allo! Event Page for Browser Action');
