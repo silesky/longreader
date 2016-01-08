bb1a0ec32bb64509cf309b218500801be1c74ec6
Seth Silesky <ssilesky@Seths-Tulane-Macbook.local>
2016-01-08 15:53:29 -0600

    added jquery, still getting CSP error

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -6,11 +6,34 @@ chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.executeScript({
     code: 'document.body.style.backgroundColor="red"'
   });
-  function getTabUrl() {
-    console.log(tab.url);
-  }
-  chrome.tabs.executeScript(getTabUrl());
 
+  function getTabUrl() {
+    console.log("Tab URL is: " + tab.url);
+  }
+
+  getTabUrl();
+  var key = "7fe8d00774cd51911b4cce37206c0832a42b3348";
+
+  var testUrl = "https://en.wikipedia.org/wiki/Igneous_rock";
+
+  var myApi = "https://readability.com/api/content/v1/parser?url=" + testUrl + "&token=" + key;
+  $('document').ready(function(){
+    $.ajax({
+      type: "Get",
+      url: myApi,
+      async: false,
+      contentType: "application/json",
+      dataType: 'jsonp',
+      success: function(e) {
+        if (e) {
+          console.log(e.url);
+          console.log(e.content);
+        } else {
+          console.log("error");
+        }
+      }
+    });
+  });
 });
 
 /* this is like the controller */
