5b1e9904cf2cb6a714d90803ae60926d4d90d859
seth <seth@mail.org>
2016-01-10 15:23:24 -0600

    refactored stuff... console.log works

--- app/scripts/inject.js
+++ app/scripts/inject.js
@@ -1,22 +1,27 @@
 document.body.style.backgroundColor="yellow";
 console.log("CONTENT SCRIPT: " + window.location.href);
-var key = '7fe8d00774cd51911b4cce37206c0832a42b3348';
-var testUrl = 'https://en.wikipedia.org/wiki/Igneous_rock';
-var myApi = 'https://readability.com/api/content/v1/parser?url=' + testUrl + '&token=' + key;
-$('document').ready(function(){
-  $.ajax({
-    type: 'Get',
-    url: myApi,
-    async: false,
-    contentType: 'application/json',
-    dataType: 'json',
-    success: function(e) {
-      if (e) {
-        console.log('jsonp call successful: ' + e.url);
-        // console.log(e.content);
-      } else {
-        console.log('error');
+
+var init = function() {
+  var key = '7fe8d00774cd51911b4cce37206c0832a42b3348';
+  var currentUrl = window.location.href;
+  var myApi = 'https://readability.com/api/content/v1/parser?url=' + currentUrl + '&token=' + key;
+  $('document').ready(function(){
+    $.ajax({
+      type: 'Get',
+      url: myApi,
+      async: false,
+      contentType: 'application/json',
+      dataType: 'json',
+      success: function(e) {
+        if (e) {
+          console.log('jsonp call successful: ' + e.url);
+          console.log(e.content);
+        } else {
+          console.log('error');
+        }
       }
-    }
+    });
   });
-});
+};
+
+init();
