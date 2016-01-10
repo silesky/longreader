a34779ad457d40a394b8d74d65811f5b35f12834
seth <seth@mail.org>
2016-01-10 15:57:01 -0600

    more refactoring

--- app/scripts/inject.js
+++ app/scripts/inject.js
@@ -1,12 +1,12 @@
-document.body.style.backgroundColor="yellow";
-console.log("CONTENT SCRIPT: " + window.location.href);
+var getHtmlArticleFromCurrentUrl;
+document.body.style.backgroundColor='yellow';
+console.log('CONTENT SCRIPT: ' + window.location.href);
 
-var init = function() {
+var getHtmlArticleFromCurrentUrl = function() {
   var key = '7fe8d00774cd51911b4cce37206c0832a42b3348';
   var currentUrl = window.location.href;
   var myApi = 'https://readability.com/api/content/v1/parser?url=' + currentUrl + '&token=' + key;
-  $('document').ready(function(){
-    $.ajax({
+  $.ajax({
       type: 'Get',
       url: myApi,
       async: false,
@@ -15,13 +15,13 @@ var init = function() {
       success: function(e) {
         if (e) {
           console.log('jsonp call successful: ' + e.url);
+          //console.log(e.content);
           console.log(e.content);
         } else {
           console.log('error');
         }
       }
     });
-  });
 };
 
-init();
+getHtmlArticleFromCurrentUrl();
