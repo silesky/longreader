5c88769d39b3b5101670bd7542223e023a9baa1e
seth <seth@mail.org>
2016-01-10 15:15:19 -0600

    able to get current url in content script

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,6 +1,7 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
   // No tabs or host permissions needed!
   console.log('Turning ' + tab.url + ' yellow!');
+  /* using this method turns the following scripts into "content scripts" */
   chrome.tabs.executeScript(null, {file: 'scripts/libraries/jquery-2.2.0.min.js'});
   chrome.tabs.executeScript(null, {file: 'scripts/inject.js'});
 
--- app/scripts/inject.js
+++ app/scripts/inject.js
@@ -1,4 +1,5 @@
 document.body.style.backgroundColor="yellow";
+console.log("CONTENT SCRIPT: " + window.location.href);
 var key = '7fe8d00774cd51911b4cce37206c0832a42b3348';
 var testUrl = 'https://en.wikipedia.org/wiki/Igneous_rock';
 var myApi = 'https://readability.com/api/content/v1/parser?url=' + testUrl + '&token=' + key;
