1e14975af8bb8312264933ff1c1975ce5f76a692
seth <seth@mail.org>
2016-01-10 11:59:08 -0600

    jquery works, but getting Uncaught ReferenceError:

--- app/scripts/inject.js
+++ app/scripts/inject.js
@@ -1,1 +1,21 @@
 document.body.style.backgroundColor="yellow";
+var key = '7fe8d00774cd51911b4cce37206c0832a42b3348';
+var testUrl = 'https://en.wikipedia.org/wiki/Igneous_rock';
+var myApi = 'https://readability.com/api/content/v1/parser?url=' + testUrl + '&token=' + key;
+$('document').ready(function(){
+  $.ajax({
+    type: 'Get',
+    url: myApi,
+    async: false,
+    contentType: 'application/json',
+    dataType: 'jsonp',
+    success: function(e) {
+      if (e) {
+        console.log('jsonp call successful: ' + e.url);
+        // console.log(e.content);
+      } else {
+        console.log('error');
+      }
+    }
+  });
+});
