415b7908218d1547a3f12b4956446f13b9384ce6
Seth Silesky <ssilesky@Seths-Tulane-Macbook.local>
2016-01-14 14:27:17 -0600

    refactored into revealing module pattern

--- app/scripts/inject.js
+++ app/scripts/inject.js
@@ -2,33 +2,45 @@ var Module = (function() {
   var key = '7fe8d00774cd51911b4cce37206c0832a42b3348';
   var currentUrl = window.location.href;
   var myApi = 'https://readability.com/api/content/v1/parser?url=' + currentUrl + '&token=' + key;
-  var articleText;
+  var getArticleText = function() {
+        $.ajax({
+            type: 'Get',
+            url: myApi,
+            async: false,
+            contentType: 'application/json',
+            dataType: 'json',
+            success: function(response) {
+                if (response) {
+                    console.log('jsonp call successful: ' + response.url);
+                    articleText = response.content;
+
+                } else {
+                    console.log('error');
+                }
+            } //end of success callback
+        }); //end of ajax call
+        return articleText;
+    }; //end of getArticleText()
+  var displayContent = function() {
+      var text = getArticleText();
+      $('body').html(text);
+  };
+  var bindBackButton = function() {
+      $('#handler').on('click', function () {
+        var oldContent = $('body').html();
+        $('body').html(oldContent);
+        $('body').prepend('<a id="handler">BACK BUTTON</a>');
+      });
+  };
   return {
-   getArticleText: function() {
-      $.ajax({
-      type: 'Get',
-      url: myApi,
-      async: false,
-      contentType: 'application/json',
-      dataType: 'json',
-      success: function(response) {
-        if (response) {
-          console.log('jsonp call successful: ' + response.url);
-          articleText = response.content;
-
-        } else {
-          console.log('error');
-        }
-      } //end of success callback
-     }); //end of ajax call
-     return articleText;
-   } //end of callAjax
-
+    init: function() {
+      displayContent();
+      bindBackButton();
+    }
   }; //end of return
-})();
 
-var text = Module.getArticleText();
-console.log(" # ## ### TEXT IS ### ## # " + text);
-var oldContents = $('body').html();
-$('body').html(text);
-$('body').prepend('<a id="handler">BACK BUTTON</a>');
+})();
+Module.init();
+
+
+
