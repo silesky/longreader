e81dca095b92cdc1f245ca0561f647486a802c2d
seth <seth@mail.org>
2016-01-24 08:06:37 -0600

    added slideToggle

--- app/scripts/inject.js
+++ app/scripts/inject.js
@@ -1,23 +1,28 @@
 var Module = (function() {
+
   var key = '7fe8d00774cd51911b4cce37206c0832a42b3348';
   var currentUrl = window.location.href;
   var myApi = 'https://readability.com/api/content/v1/parser?url=' + currentUrl + '&token=' + key;
   var oldContent = $('html').html();
   var displayTemplate = function() {
     var source =
-            '<!doctype html>' +
-            '<html>' +
-            '<body class="longreader">' +
+      '<!doctype html>' +
+        '<html>' +
+          '<body class="longreader">' +
             '<nav>' +
-            '<button type="submit" id="longreader-back-btn" onClick="history.go(0);"></button>' +
-            '<button type="submit" id="longreader-option-btn">OPTIONS</button>' +
+              '<button type="submit" id="longreader-back-btn" onClick="history.go(0);"></button>' +
+              '<button type="submit" id="longreader-option-btn">OPTIONS</button>' +
+                '<div id="longreader-option-window">' +
+                '<div id="longreader-option-color">Color</div>' +
+                '<div id="longreader-option-size">Font Size</div>' +
+              '</div>' +
             '</nav>' +
             '<article id="content" class="longreader-three-col">'+
               '<h1 id="title"></h1>' +
               '<h2 id="author"></h2>' +
             '</article>' +
-            '</body>' +
-            '</html>';
+          '</body>' +
+        '</html>';
     $('html').html(source);
   };
   var getArticle = function() {
@@ -48,14 +53,23 @@ var Module = (function() {
       $('#author').append(author);
       $('#content').append(content);
   };
+  var bindSettings = function() {
+      $('#longreader-option-btn').on('click', function() {
+        $('#longreader-option-window').slideToggle('slow', function() {
+        });
+      });
+  };
+
   var bindBackButton = function() {
       $('#longreader-back-btn').html('BACK'); //doesn't work bc of message passing
   };
+
   return {
     init: function() {
       displayTemplate();
       displayContent();
       bindBackButton();
+      bindSettings();
     }
   }; //end of return
 
