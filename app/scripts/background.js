125ddb0d93318f63bcc6ba904590d7b22e12ef0f
seth <seth@mail.org>
2016-01-24 06:57:24 -0600

    cleaned up background page

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,16 +1,10 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-  // No tabs or host permissions needed!
-  console.log('Turning ' + tab.url + ' yellow!');
-  /* using this method turns the following scripts into "content scripts" */
+    /* using this method turns the following scripts into "content scripts" */
   chrome.tabs.executeScript(null, {file: 'scripts/libraries/handlebars.min.js'});
   chrome.tabs.executeScript(null, {file: 'scripts/libraries/jquery-2.2.0.min.js'});
   chrome.tabs.executeScript(null, {file: 'scripts/inject.js'});
   chrome.tabs.insertCSS(null, {file: 'scripts/inject.css'});
-  function getTabUrl() {
-      console.log('Tab URL is: ' + tab.url);
-  }
 
-  getTabUrl();
 
 
 });
