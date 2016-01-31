d14247828d3bd6910b678fd26749a202d39d828e
silesky <seth.silesky@gmail.com>
2016-01-31 18:52:27 -0500

    added knockout

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,7 +1,8 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
     /* using this method turns the following scripts into "content scripts" */
 
-  chrome.tabs.executeScript(null, {file: 'scripts/libraries/handlebars.min.js'});
+
+  chrome.tabs.executeScript(null, {file: 'scripts/libraries/node_modules/knockout/build/output/knockout-latest.js'});
   chrome.tabs.executeScript(null, {file: 'scripts/libraries/jquery-2.2.0.min.js'});
   chrome.tabs.executeScript(null, {file: 'scripts/plugins/tinyColorPicker/jqColorPicker.min.js'});
   chrome.tabs.executeScript(null, {file: 'scripts/inject.js'});
--- app/scripts/inject.js
+++ app/scripts/inject.js
@@ -1,145 +1,123 @@
 /*global $:false */
-var LONGREADER = (function() {
+(function() {
+  'use strict';
 
   var key = '7fe8d00774cd51911b4cce37206c0832a42b3348';
   var currentUrl = window.location.href;
   var myApi = 'https://readability.com/api/content/v1/parser?url=' + currentUrl + '&token=' + key;
   var article;
   var MODEL = (function() {
-      var getArticle = function () {
-          $.ajax({
-              type: 'Get',
-              url: myApi,
-              async: false,
-              contentType: 'application/json',
-              dataType: 'json',
-              success: function (response) {
-                  if (response) {
-                      console.log('jsonp call successful: ' + response.url);
-                      article = response;
-                  } else {
-                      console.log('error');
-                  }
-              } //end of success callback
-          }); //end of ajax call
-          return article;
-      };
-      return {
-            getArticle: getArticle
-      }; //end of getArticle()
-  })();
-
-  var VIEW = (function() {
-      var displayTemplate = function () {
-          var source =
-              '<!doctype html>' +
-              '<html>' +
-              '<body class="longreader">' +
-              '<nav>' +
-              '<button type="submit" id="longreader-back-btn"></button>' +
-              '<button type="submit" id="longreader-option-btn">OPTION</button>' +
-              '<div id="longreader-option-window" class="longreader-option">' +
-              '<input id="longreader-colorpicker-bg" placeholder="Background Color" size="18" class="color"></input>' +
-              '<input id="longreader-colorpicker-fontcolor" placeholder="Font Color" size="18" class="color"></input>' +
-              '<input id="longreader-fontsize" placeholder="Font" size="18"></input>' +
-              '<input id="longreader-option-size" placeholder="Text Size" size="18"></input>' +
-              '</div>' +
-              '</nav>' +
-              '<article id="content" class="longreader-three-col">' +
-              '<h1 id="title"></h1>' +
-              '<h2 id="author"></h2>' +
-              '</article>' +
-              '</body>' +
-              '</html>';
-          $('html').html(source);
-      };
-      var displayContent = function() {
-          var articleObj = MODEL.getArticle();
-          var content = articleObj.content;
-          var author = articleObj.author;
-          // var url = articleObj.url;
-          var title = articleObj.title;
-          $('#title').append(title);
-          $('#author').append(author);
-          $('#content').append(content);
-      };
-      var settingsBar = {
-          showSettingsBar: function() {
-            var cogIcon = chrome.extension.getURL('images/cog.png');
-            $('#longreader-option-btn').html('<img src=' + cogIcon + ' style="height:1.5em;width:1.5em" />');
-            $('#longreader-option-btn').on('click', function () {
-                $('#longreader-option-window').slideToggle('slow');
-            });
-          },
-          showBackButton: function() {
-              var backIcon = chrome.extension.getURL('images/back-white.svg');
-              $('#longreader-back-btn').html('<img src=' + backIcon + ' style="height:1.5em;width:1.5em" />');
-              $('#longreader-back-btn').on('click', function () {
-                  location.reload();
-              });
-          },
-          bindColorPicker: function() {
-
-            /* callback */
-
-
-             var openColorWidgetGetColor = function(selector) {
-               // var fontColors = $('input#longreader-colorpicker-fontcolor').colorPicker();
-               var colors = $(selector).colorPicker();
-               console.log("success: " + colors[0].value);
-               return colors[0].value;
-             }
-             /* when you click the form input, the pallet opens */
-            var bindPallet = function() {
-              $('#longreader-colorpicker-bg').on('focusin', openColorWidgetGetColor('input#longreader-colorpicker-bg'));
-              $('#longreader-colorpicker-fontcolor').on('focusin', openColorWidgetGetColor('input#longreader-colorpicker-fontcolor'));
-            }; //end of bindPallet
-
-
-            /* when you click the pallet widget, grab the color and change the background*/
-              var bindClickEvent  = function(currentFontColor, currentColor) {
-                $(document).on('click', function() {
-                  var currentFontColor = openColorWidgetGetColor('input#longreader-colorpicker-fontcolor');
-                  $('body').css('background-color', currentColor);
-                  $('body').css('color', currentFontColor);
-                  });
-              }; //end of bindClickEvent
-
-              /* when you enter in data from the from, grab the color */
-              var bindKeyPressEvent = function() {
-                  $(this).keypress(function() {
-                    console.log('keypress: ' + getCurrentColor());
-                  });
-              };
-
-              bindPallet();
-              bindClickEvent();
-              bindKeyPressEvent();
-
-              }
-          }; //end of bindColorPicker function
-       //end of bindColorPicker object literal
-      return {
-          displayTemplate: displayTemplate,
-          displayContent: displayContent,
-          displaySettingsBar: function() {
-              settingsBar.showSettingsBar();
-              settingsBar.showBackButton();
-              settingsBar.bindColorPicker();
+    var getArticle = function () {
+      $.ajax({
+        type: 'Get',
+        url: myApi,
+        async: false,
+        contentType: 'application/json',
+        dataType: 'json',
+        success: function (response) {
+          if (response) {
+            console.log('jsonp call successful: ' + response.url);
+            article = response;
+          } else {
+            console.log('error');
           }
-      };
+        } //end of success callback
+      }); //end of ajax call
+      return article;
+    };
+    return {
+      getArticle: getArticle
+    }; //end of getArticle()
   })();
 
+  var displayTemplate = function () {
+    var templateUri = chrome.extension.getURL('views/template.html');
+    return $.ajax({
+      url: templateUri,
+    })
+    .done(function (data) {
+      $('html').html(data);
+    });
+  };
+
+  var displayContent = function() {
+    var articleObj = MODEL.getArticle();
+    var content = articleObj.content;
+    var author = articleObj.author;
+    // var url = articleObj.url;
+    var title = articleObj.title;
+    $('#title').append(title);
+    $('#author').append(author);
+    $('#content').append(content);
+  };
+  var settingsBar = {
+    showSettingsBar: function() {
+      var cogIcon = chrome.extension.getURL('images/cog.png');
+      $('#longreader-option-btn').html('<img src=' + cogIcon + ' style="height:1.5em;width:1.5em" />');
+      $('#longreader-option-btn').on('click', function () {
+        $('#longreader-option-window').slideToggle('slow');
+      });
+    },
+    showBackButton: function() {
+      var backIcon = chrome.extension.getURL('images/back-white.svg');
+      $('#longreader-back-btn').html('<img src=' + backIcon + ' style="height:1.5em;width:1.5em" />');
+      $('#longreader-back-btn').on('click', function () {
+        location.reload();
+      });
+    },
+    bindColorPicker: function() {
+
+      /* callback */
 
 
-  return {
-    init: function() {
-      MODEL.getArticle();
-      VIEW.displayTemplate();
-      VIEW.displayContent();
-      VIEW.displaySettingsBar();
+      var openColorWidgetGetColor = function(selector) {
+        // var fontColors = $('input#longreader-colorpicker-fontcolor').colorPicker();
+        var colors = $(selector).colorPicker();
+        console.log('success: ' + colors[0].value);
+        return colors[0].value;
+      };
+
+      /* when you click the form input, the pallet opens */
+      var bindPallet = function() {
+        $('#longreader-colorpicker-bg').on('focusin', openColorWidgetGetColor('input#longreader-colorpicker-bg'));
+        $('#longreader-colorpicker-fontcolor').on('focusin', openColorWidgetGetColor('input#longreader-colorpicker-fontcolor'));
+      }; //end of bindPallet
+
+
+      /* when you click the pallet widget, grab the color and change the background*/
+      var bindClickEvent  = function() {
+        $(document).on('click', function() {
+          var currentFontColor = openColorWidgetGetColor('input#longreader-colorpicker-fontcolor');
+          var currentBgColor = openColorWidgetGetColor('input#longreader-colorpicker-bg');
+          $('body').css('background-color', currentBgColor);
+          $('body').css('color', currentFontColor);
+        });
+      }; //end of bindClickEvent
+
+      /* when you enter in data from the from, grab the color */
+      var bindKeyPressEvent = function() {
+        $(this).keypress(function() {
+        });
+      };
+
+      bindPallet();
+      bindClickEvent();
+      bindKeyPressEvent();
+
     }
-  }; //end of return
+  }; //end of bindColorPicker function
+
+  var longreaderVm = {
+    init: function() {
+      displayTemplate().done(function () {
+        displayContent();
+        settingsBar.showSettingsBar();
+      });
+
+      return longreaderVm;
+    },
+  };
+
+  ko.applyBindings(longreaderVm.init());
 
 })();
-LONGREADER.init();
