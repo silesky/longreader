e8e71509b27300877a93bcd44466d03a7efdc930
Seth Silesky <ssilesky@Seths-Tulane-Macbook.local>
2016-02-01 14:02:58 -0600

    bg color workingish

--- app/scripts/inject.js
+++ app/scripts/inject.js
@@ -2,16 +2,21 @@
   (function() {
     'use strict';
 
-  ko.bindingHandlers.colorPicker = {
-    init: function (element, valueAccessor) {
-      var $el = $(element);
-      $el.colorPicker();
-
-    },
-  };
+  // ko.bindingHandlers.colorPicker = {
+  //   init: function (element, valueAccessor) {
+  //     $('input.color').colorPicker();
+  //   },
+  //   update: function(element, valueAccessor) {
+  //     var value = valueAccessor();
+  //     console.log(value);
+  //
+  //     var myColors = $(element).colorPicker();
+  //     console.log(myColors[0].value);
+  //   }
+  // };
 
     // Observables
-    var bgColor = ko.observable();
+    var bgColorWidget = ko.observable();
     var fontColor = ko.observable();
 
     var key = '7fe8d00774cd51911b4cce37206c0832a42b3348';
@@ -58,23 +63,15 @@
       var articleContent = articleObj.content;
       var articleTitle = articleObj.title;
       var articleAuthor = articleObj.author;
-      var speak = function() {
-        console.log("THE TITLE:" + articleObj.title);
-      }
-      // var url = articleObj.url;
-      //$('#title').append(title);
-       // $('#author').append(author);
-      // $('#content').append(content);
       return {
-
         articleContent: articleContent,
         articleTitle: articleTitle,
-        articleAuthor: articleAuthor,
-        speak: speak
-      }
+        articleAuthor: articleAuthor
+      };
     })();
 
     var settingsBar = {
+
       slideUpDown: function() {
         console.log('slide up down');
         $('#longreader-option-window').slideToggle('slow');
@@ -82,45 +79,41 @@
       reloadPage: function() {
           location.reload();
       },
-      bindColorPicker: function() {
+      bgColorWidget: function(data, event) {
+            var currentID = event.target.id;
+            console.log(data);
+            console.log(event.target.id);
+            var currentID = event.target.id;
+            $('#' + currentID).colorPicker();
 
-        var openColorWidgetGetColor = function(selector) {
-          var colors = $(selector).colorPicker();
-          console.log('success: ' + colors[0].value);
-          return colors[0].value;
-        };
+        },
 
         /* when you click the form input, the pallet opens */
-        var bindPallet = function() {
-          $('#longreader-colorpicker-bg').on('focusin', openColorWidgetGetColor('input#longreader-colorpicker-bg'));
-          $('#longreader-colorpicker-fontcolor').on('focusin', openColorWidgetGetColor('input#longreader-colorpicker-fontcolor'));
-        }; //end of bindPallet
+        // bindPallet: function() {
+        //   $('#longreader-colorpicker-bg').on('focusin', openColorWidgetGetColor('input#longreader-colorpicker-bg'));
+        //   $('#longreader-colorpicker-fontcolor').on('focusin', openColorWidgetGetColor('input#longreader-colorpicker-fontcolor'));
+        // }, //end of bindPallet
 
         /* when you click the pallet widget, grab the color and change the background*/
-        var bindClickEvent  = function() {
-          $(document).on('click', function() {
-            var currentFontColor = openColorWidgetGetColor('input#longreader-colorpicker-fontcolor');
-            var currentBgColor = openColorWidgetGetColor('input#longreader-colorpicker-bg');
-            $('body').css('background-color', currentBgColor);
-            $('body').css('color', currentFontColor);
-          });
-        }; //end of bindClickEvent
+        // bindClickEvent: function() {
+        //   $('.cp-xy-cursor').on('change', function() {
+        //     var currentFontColor = openColorWidgetGetColor('input#longreader-colorpicker-fontcolor');
+        //     var currentBgColor = openColorWidgetGetColor('input#longreader-colorpicker-bg');
+        //     $('body').css('background-color', currentBgColor);
+        //     $('body').css('color', currentFontColor);
+        //   });
+        // }, //end of bindClickEvent
 
         /* when you enter in data from the from, grab the color */
-        var bindKeyPressEvent = function() {
+        bindKeyPressEvent: function() {
           $(this).keypress(function() {
           });
-        };
-
-        // bindPallet();
-        // bindClickEvent();
-        // bindKeyPressEvent();
-
-      },
-    }; //end of bindColorPicker function
+        }
+    };
 
     var longreaderVm = {
-      init: function() {
+      init:
+      function() {
         return $.when(displayTemplate()).done(function() {
           console.log("displayTemplate() finished!");
         });
@@ -129,18 +122,16 @@
       cogIconUri: chrome.extension.getURL('images/cog.png'),
       backIconUri: chrome.extension.getURL('images/back-white.svg'),
       settingsBar: settingsBar,
-      bgColor: bgColor,
-      fontColor: fontColor,
+      bgColorWidget: settingsBar.bgColorWidget,
       articleContent: displayContent.articleContent,
       articleTitle: displayContent.articleTitle,
       articleAuthor: displayContent.articleAuthor
-    };
+  };
 
     longreaderVm
       .init()
       .done(function () {
         console.log("init done");
-        displayContent.speak();
         ko.applyBindings(longreaderVm);
       });
 
