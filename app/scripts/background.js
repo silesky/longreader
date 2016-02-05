46ae56abc1dfe68e685b604e5a133cd3515baae0
Seth Silesky <ssilesky@Seths-Tulane-Macbook.local>
2016-02-05 13:53:40 -0600

    deleted extraneous scripts

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,17 +1,12 @@
+
 chrome.browserAction.onClicked.addListener(function(tab) {
     /* using this method turns the following scripts into "content scripts" */
-
-
   chrome.tabs.executeScript(null, {file: '/node_modules/knockout/build/output/knockout-latest.js'});
   chrome.tabs.executeScript(null, {file: 'scripts/libraries/jquery-2.2.0.min.js'});
   chrome.tabs.executeScript(null, {file: 'scripts/plugins/tinyColorPicker/jqColorPicker.min.js'});
   chrome.tabs.executeScript(null, {file: 'scripts/inject.js'});
   chrome.tabs.insertCSS(null, {file: 'scripts/inject.css'});
-  chrome.tabs.executeScript(null, {file: 'scripts/libraries/jquery-ui.min.js'});
-  chrome.tabs.executeScript(null, {file: 'scripts/plugins/jquery-ui-fontselector/jquery.ui.fontSelector.js'});
-
-
-
+  });
 });
 
 /* this is like the controller */
