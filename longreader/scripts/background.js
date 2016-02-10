e8df5a757d8f28ca2ba7e621d37beeadfbbd4e70
Seth Silesky <ssilesky@Seths-Tulane-Macbook.local>
2016-02-10 12:37:12 -0600

    changed script order

--- longreader/scripts/background.js
+++ longreader/scripts/background.js
@@ -3,9 +3,9 @@ chrome.browserAction.onClicked.addListener(function(tab) {
     /* using this method turns the following scripts into "content scripts" */
   chrome.tabs.executeScript(null, {file: 'scripts/libraries/knockout-3.4.0.js'});
   chrome.tabs.executeScript(null, {file: 'scripts/libraries/jquery-2.2.0.min.js'});
-  chrome.tabs.executeScript(null, {file: 'scripts/plugins/jqColorPicker.min.js'});
   chrome.tabs.executeScript(null, {file: 'scripts/inject.js'});
   chrome.tabs.insertCSS(null, {file: 'scripts/inject.css'});
+  chrome.tabs.executeScript(null, {file: 'scripts/plugins/jqColorPicker.min.js'});
 });
 
 /* this is like the controller */
