77f5408a92e1be402baecfbfa0ad0bf06ecc18e8
Seth Silesky <ssilesky@Seths-Tulane-Macbook.local>
2016-03-04 14:19:04 -0600

    added context menu logic... 16px icon needs some work

--- longreader/scripts/background.js
+++ longreader/scripts/background.js
@@ -1,9 +1,19 @@
-
-chrome.browserAction.onClicked.addListener(function(tab) {
-    /* using this method turns the following scripts into "content scripts" */
+var executeScript = function() {
   chrome.tabs.executeScript(null, {file: 'scripts/libraries/knockout-3.4.0.js'});
   chrome.tabs.executeScript(null, {file: 'scripts/libraries/jquery-2.2.0.min.js'});
   chrome.tabs.executeScript(null, {file: 'scripts/inject.js'});
   chrome.tabs.insertCSS(null, {file: 'scripts/inject.css'});
   chrome.tabs.executeScript(null, {file: 'scripts/plugins/jqColorPicker.min.js'});
+};
+
+var createContextMenu = function() {
+  chrome.contextMenus.create({title: 'Longreader', id: 'parent', onclick: executeScript() });
+};
+
+chrome.runtime.onInstalled.addListener(function() {
+  createContextMenu();
+});
+
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+  executeScript()
 });
